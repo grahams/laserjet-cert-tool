81ed26fab7cc32f290cbf68f3e68944c67ad6886
Sean Graham <github@sean-graham.com>
2025-08-27 01:41:59 +0000

    removed _s from envs, added deploy hook env

--- renewAcmeCert.sh
+++ renewAcmeCert.sh
@@ -9,30 +9,30 @@ else
 fi
 
 # Main
-export PATH=${PATH}:${_ACME_DIR}:${_BASE_DIR}
-chmod +x ${_ACME_DIR}/acme.sh
-mkdir -p ${_CERT_DIR}
+export PATH=${PATH}:${ACME_DIR}:${BASE_DIR}
+chmod +x ${ACME_DIR}/acme.sh
+mkdir -p ${CERT_DIR}
 
 # Main
-mkdir -p ${_CERT_DIR}
+mkdir -p ${CERT_DIR}
 
-if [ -d "$_CERT_DIR" ]; then
+if [ -d "$CERT_DIR" ]; then
     # Renew certificate
-    acme.sh --renew -d ${_DOMAIN_NAME}
+    acme.sh --renew -d ${DOMAIN_NAME}
 else
     # Issue new certificate
-    acme.sh --issue -k 4096 -ak 4096 --server letsencrypt --force --dns dns_namecheap --cert-file "${_CERT_FILE}" --key-file "${_KEY_FILE}" --ca-file "${_CA_FILE}" --fullchain-file "${_FULLCHAIN_FILE}" -d ${_DOMAIN_NAME}
+    acme.sh --issue -k 4096 -ak 4096 --server letsencrypt --force --dns ${ACME_DEPLOY_HOOK} --cert-file "${CERT_FILE}" --key-file "${KEY_FILE}" --ca-file "${CA_FILE}" --fullchain-file "${FULLCHAIN_FILE}" -d ${DOMAIN_NAME}
 fi
 
 # Package certificate
-openssl pkcs12 -export -out ${_PFX_FILE} -passout pass:${PFX_PASSWORD} -inkey ${_KEY_FILE} -in ${_CERT_FILE}
+openssl pkcs12 -export -out ${PFX_FILE} -passout pass:${PFX_PASSWORD} -inkey ${KEY_FILE} -in ${CERT_FILE}
 
 # Deploy certificate
 status=$(curl -ks -u "$PRINTER_USERNAME:$PRINTER_PASSWORD" \
-  -F "certificate=@${_PFX_FILE}" \
+  -F "certificate=@${PFX_FILE}" \
   -F "password=${PFX_PASSWORD}" \
   -w "%{http_code}" \
-  "https://${_DOMAIN_NAME}/Security/DeviceCertificates/NewCertWithPassword/Upload")
+  "https://${DOMAIN_NAME}/Security/DeviceCertificates/NewCertWithPassword/Upload")
 
 if [ "$status" != "201" ]; then
     echo "Error (Status Code: $status)"
